92bef6e0801e8ef53f64c22464d9d6f85c2ec7ed
Jason Zhuang <zhuangkaizhi@gmail.com>
2022-02-14 22:25:05 +1100

    update

---
+++ UsingMavenAssemblyPlugin/src/main/java/com/jasonz/usingMavenAssemblyPlugin/UsingMavenAssemblyPluginApplication.java
@@ -0,0 +1,13 @@
+package com.jasonz.usingMavenAssemblyPlugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingMavenAssemblyPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingMavenAssemblyPluginApplication.class, args);
+	}
+
+}
---
+++ UsingMavenAssemblyPlugin/src/test/java/com/jasonz/usingMavenAssemblyPlugin/UsingMavenAssemblyPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasonz.usingMavenAssemblyPlugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsingMavenAssemblyPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
