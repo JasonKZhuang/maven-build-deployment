c2eed55570ed639c4584fce47c7df5354ee220b8
Jason Zhuang <zhuangkaizhi@gmail.com>
2022-02-14 22:09:32 +1100

    update

---
+++ UsingMavenJarPlugin/src/main/java/com/jasonz/usingMavenJarPlugin/UsingMavenJarPluginApplication.java
@@ -0,0 +1,13 @@
+package com.jasonz.usingMavenJarPlugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingMavenJarPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingMavenJarPluginApplication.class, args);
+	}
+
+}
---
+++ UsingMavenJarPlugin/src/test/java/com/jasonz/usingMavenJarPlugin/UsingMavenJarPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasonz.usingMavenJarPlugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsingMavenJarPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
